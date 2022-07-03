e29bd115628167b862d09bc07b9c80da588d0ffd
Solal Fitoussi <solal.fitoussi@gmail.com>
2022-07-03 18:59:24 +0000

    ading missing bs4 requirement

--- setup.py
+++ setup.py
@@ -5,6 +5,7 @@ setup(
     version='0.1',
     py_modules=['devsearch'],
     install_requires=[
+        'bs4>=0.0.1'
         'click>=8.1.3',
         'rich>=12.4.1',
         'prompt-toolkit>=3.0.29'
